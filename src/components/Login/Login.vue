4e211cc5dac4ef8e5bca40374d789f92fcee664b
clayton <crdcastro5@gmail.com>
2024-01-02 21:49:00 -0800

    add: basic login form (non-functional)

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+    import Form from './Form.vue'
+    import Title from './Title.vue'
+</script>
+
+<template>
+  <div>
+      <Title title="RGS Carbide Images"/>
+      <Form/>
+  </div>
+</template>
---
+++ src/components/Login/Title.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+<h1>{{ title }}</h1>
+</template>
+
+
+<style scoped>
+h1 {
+    font-weight: 200;
+    font-size: 3.4rem;
+    position: relative;
+}
+</style>
