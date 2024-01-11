54a30076ba0b0e177480a4b662348522c2e0c27d
clayton <crdcastro5@gmail.com>
2024-01-10 21:21:38 -0800

    update: products component now resides in parent dashboard component
    - it also utilizes a real network request instead of dummy data

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    import Products from './dashboard/products.vue'
+</script>
+
+<template>
+    <Suspense>
+        <template #default>
+            <Products/>
+        </template>
+        <template #fallback>
+            <span>Loading...</span>
+        </template>
+    </Suspense>
+</template>
---
+++ src/components/dashboard/products.vue
@@ -0,0 +1,37 @@
+<template>
+ <div>
+   <div v-for="product in products" :key="product.Id" class="product-icon">
+     <i class="fas fa-cube"></i>
+     <p>{{ product.Name }}</p>
+   </div>
+ </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { getAllProducts } from '../../services/product'
+
+const products = ref<Product[]>([])
+
+try {
+    products.value = await getAllProducts()
+    console.log(products.value)
+} catch (error) {
+    console.error("error:", error);
+    // (document.getElementById("error-message") as HTMLParagraphElement).innerText = "error:" + error.message;
+}
+</script>
+
+<style scoped>
+.release-icon {
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+ margin: 10px;
+}
+
+.fa-cube {
+ font-size: 24px;
+ margin-bottom: 5px;
+}
+</style>
