7db72ba6fc6d3be9e71e55fbd31c8773b6b540e1
clayton <crdcastro5@gmail.com>
2024-01-10 21:18:35 -0800

    chore: add type definitions for objects

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,39 @@
 /// <reference types="vite/client" />
+
+interface Product {
+    Id: number;
+    Name: string;
+    CreatedAt: string;
+    UpdatedAt: string;
+}
+
+interface Release {
+    Id: number;
+    ProductId: number;
+    Name: string;
+    TarballLink: string;
+    CreatedAt: string;
+    UpdatedAt: string;
+    // Images: null;
+}
+
+interface Image {
+    Id: number;
+    ImageName: string;
+    ImageSigned: boolean;
+    TrivySigned: boolean;
+    TrivyValid: boolean;
+    SbomSigned: boolean;
+    SbomValid: boolean;
+    LastScannedAt: string;
+    CreatedAt: string;
+    UpdatedAt: string;
+}
+
+interface ReleaseImageMapping {
+    Id: number;
+    ReleaseId: number;
+    ImageId: number;
+    CreatedAt: string;
+    UpdatedAt: string;
+}
