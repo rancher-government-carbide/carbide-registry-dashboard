63a27fa8d5e694f5f8c0bf8e84acdaa4a201421a
clayton <crdcastro5@gmail.com>
2024-05-26 02:38:13 -0700

    navbar logo now redirects to main dashboard

--- components/navbar.vue
+++ components/navbar.vue
@@ -7,9 +7,9 @@ defineProps<{
 <template>
     <div class="navbar justify-content-center bg-body-tertiary">
         <div class="container">
-            <a class="navbar-brand" href="#">
-              <img src="../assets/RGS_Black-pos_sRGB.svg" alt="Logo" height="70" class="d-inline-block align-text-top"</img>
-            </a>
+            <Nuxt-Link class="navbar-brand" to="/dashboard">
+                <img src="../assets/RGS_Black-pos_sRGB.svg" alt="Logo" height="70" class="d-inline-block align-text-top"</img>
+            </Nuxt-Link>
         </div>
     </div>
 </template>
