19f4e3ee49960ca67ed360523428e8ff086521e5
clayton <crdcastro5@gmail.com>
2024-05-26 03:21:34 -0700

    add signout button and respective api calls

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,18 +1,26 @@
-<script setup lang="ts">
-defineProps<{
-  title: string
-}>()
-</script>
-
 <template>
     <div class="navbar justify-content-center bg-body-tertiary">
         <div class="container">
             <Nuxt-Link class="navbar-brand" to="/dashboard">
                 <img src="../assets/RGS_Black-pos_sRGB.svg" alt="Logo" height="70" class="d-inline-block align-text-top"</img>
             </Nuxt-Link>
+            <button class="btn btn-primary" @click="logoutButton">Sign out</button>
         </div>
     </div>
 </template>
 
-<style scoped>
-</style>
+<script setup lang="ts">
+async function logoutButton() {
+    try {
+        const success = await logout()
+        if (success) {
+            console.debug("navigating to /")
+            navigateTo('/')
+        } else {
+            console.debug("logout failed")
+        }
+    } catch (error) {
+        console.error("error:", error);
+    }
+}
+</script>
