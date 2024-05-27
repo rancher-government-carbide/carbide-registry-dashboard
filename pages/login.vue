6dfe40f7324f0ec0ff6d266534d033e3ebb5a98e
clayton <crdcastro5@gmail.com>
2024-05-27 13:49:36 -0700

    /login replaces default root redirect

---
+++ pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="absolute-center">
+        <LoginForm/>
+    </div>
+</template>
+
+<style scoped>
+.absolute-center {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
