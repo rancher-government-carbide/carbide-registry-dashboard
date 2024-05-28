7da270059cbadefdc78a881f0c33cb3e6eee42d8
clayton <crdcastro5@gmail.com>
2024-05-27 23:45:14 -0700

    transition releases to select from accordion (only display one at a time)

--- components/product/releases.vue
+++ components/product/releases.vue
@@ -1,31 +1,30 @@
 <template>
-    <div class="accordion" id="accordion">
-      <div class="accordion-item" v-for="release in releasesForProduct" :key="release.Id">
-        <h2 class="accordion-header">
-            <button class="accordion-button d-flex justify-content-between" data-bs-toggle="collapse" :data-bs-target="'#collapse'+release.Id" aria-expanded="false" :aria-controls="'collapse'+release.Id">
-                {{ release.Name }}
-                <IconsDownload :url="release.TarballLink"/>
-            </button>
-        </h2>
-        <div :id="'collapse'+release.Id" class="accordion collapse" data-bs-parent="#accordion">
-            <div class="accordion-body">
-                <ProductImages :product="productName" :release="release.Name"></ProductImages>
-            </div>
-        </div>
-      </div>
-    </div>
+    <select class="form-select form-select-lg m-2" v-model="selectedReleaseName" >
+        <option v-for="release in releasesForProduct" :value="release.Name">
+            <h2 class="d-flex justify-content-between">
+                    {{ release.Name }}
+                    <IconsDownload url="https://claytonc.dev"/>
+            </h2>
+        </option>
+    </select>
+    <ProductImages class="m-3" v-if="selectedReleaseName !== null" :key="selectedReleaseName" :product="productName" :release="selectedReleaseName"></ProductImages>
 </template>
+
 
 <script setup lang="ts">
 const route = useRoute()
 const productName = route.params.slug as string
 const releasesForProduct = ref<Release[]>([])
-try {
-    releasesForProduct.value = await getAllReleases(productName)
-    console.log(releasesForProduct.value)
-} catch (error) {
-    console.error("error:", error);
-    // (document.getElementById("error-message") as HTMLParagraphElement).innerText = "error:" + error.message;
-}
-// <td><a :href="image.TarballLink" target="_blank" class="download-button">Download</a></td>
+const selectedReleaseName = ref<string | null>(null);
+
+onMounted(async () => {
+    try {
+        releasesForProduct.value = await getAllReleases(productName);
+        if (releasesForProduct.value.length > 0) {
+            selectedReleaseName.value = releasesForProduct.value[0].Name;
+        }
+    } catch (error) {
+        console.error("error:", error);
+    }
+});
 </script>
